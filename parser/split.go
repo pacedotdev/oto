30b3ccc10145b357f4156f3b12575b6b23d83299
Mat Ryer <mat@grafana.com>
2021-12-22 14:55:37 +0000

    small lint

--- parser/split.go
+++ parser/split.go
@@ -117,7 +117,7 @@ func camelizeDown(word string) string {
 
 func isAcronym(word string) bool {
 	for _, ac := range baseAcronyms {
-		if strings.ToUpper(ac) == strings.ToUpper(word) {
+		if strings.EqualFold(ac, word) {
 			return true
 		}
 	}
